c20ab78c17530cdfb981cc85b229e5c308bc58f7
Souad <github@habentis.com>
2026-04-10 14:23:02 +0200

    rapport PowerBI

--- app/Http/Controllers/OperationalReportsController.php
+++ app/Http/Controllers/OperationalReportsController.php
@@ -17,8 +17,9 @@ class OperationalReportsController extends Controller
               
                 'title' => 'Rapport 01',
                 'description' => 'Suivi des actifs mis en rebut',
-                'src'   => 'https://app.powerbi.com/view?r=eyJrIjoiMzM4MTUxNGItN2RhOC00MWI4LTlkZmYtMmRlMTVkNTRhYWJkIiwidCI6ImZmOWJlNTQxLTVlNmItNGFmMi1hZmZmLTY5NWUyOTY4MzlmMCIsImMiOjl9'
-            ],
+                'src'   => 'https://app.powerbi.com/view?r=eyJrIjoiOTQ1ODA3MTctNjQ1Yi00ZDljLTljMGYtYmIxOWVmNjYxYzQ3IiwidCI6ImZmOWJlNTQxLTVlNmItNGFmMi1hZmZmLTY5NWUyOTY4MzlmMCIsImMiOjl9'
+                                
+                ],
           
         ];
 
